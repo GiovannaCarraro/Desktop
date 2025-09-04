d229175e3de5b990cb65e04ca8787728bc580f2c
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2025-09-04 11:26:36 -0300

    ADD: adiciomei

--- spa-cadastro-login/src/components/Button/Button.jsx
+++
@@ -1,3 +0,0 @@
-import './Button.css';
-
-export default Button;
--- spa-cadastro-login/src/components/FormularioCadastro/FormularioCadastro.jsx
+++
@@ -1,4 +0,0 @@
-import Button from '../Button/Button';
-
-// Reutiliza os mesmos estilos do formulário de login
-import '../FormularioLogin/FormularioLogin.css';
